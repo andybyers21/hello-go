fb308f3e548e23424705e0b87a9ac729c62addfa
andybyers21 <andy.byers21@gmail.com>
2021-01-16 12:53:57 +0000

    add mux dependancy & update basic server code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/andybyers21/hello-go
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 // Go code formatter to clean up any ugly indents and such.
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
+)
+
+func handler(w, http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, world!\n")
+}
+
+//Route
+func router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	return r
+}
+
+// Initiate web server
 func main() {
-	// Print “Hello, World!” to console
-	fmt.Println("Hello World")
+	router := router()
+	srv := &http.Server{
+		Handler: router,
+		Addr: "127.0.0.1:9100",
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }

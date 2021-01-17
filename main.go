8377fad628d26dddfba7a16dd32a1b43a530fd7c
andybyers21 <andy.byers21@gmail.com>
2021-01-17 13:05:35 +0000

    update running message

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func router() *mux.Router {
 
 // Initiate web server
 func main() {
-	fmt.Println(" Starting web server at `localhost:9100`.\nPress ctrl+C to close.")
+	fmt.Println("Starting web server at `localhost:9100`.\nPress ctrl+C to close.")
 	router := router()
 	srv := &http.Server{
 		Handler:      router,
